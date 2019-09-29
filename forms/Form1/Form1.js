c70c13e9b61f77800ba12e2c5d7fb2d55e0da62d
Tanner <tannerbutler15@gmail.com>
2019-09-29 17:32:50 -0500

    Done

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,9 +1,9 @@
 //1
-/*
+
 let name =prompt('What is your first name?') + prompt('What is your last name?')
 for (i =0; i<name.length; i++)
   console.log(`Letter ${[i+1]} is:' ${name.charAt(i)}`)
-*/
+
 //2
 
 let people = ['Bob', 'Janet', 'Tom', 'Bob', 'Randy', 'Elizabeth', 'Kim', 'Nancy']
@@ -12,19 +12,19 @@ for (i = 0; i < people.length; i++)
         console.log(reversePeople[i] = people[people.length - i - 1])  
 
 //3
-/*
+
 //It runs 6 times
 for (i = 0; i <= 5; i++) {
    newLabel = "another iteration in the for loop/n";
    alert(newLabel);
 };
-*/ 
+
 //4
 //This will run 5 times
-/*
+
 var x = 5;
 while (x < 10) {
     alert("The new value of X is: " + x);
     x = x + 1;
 };
-*/
+//5 - On product form
